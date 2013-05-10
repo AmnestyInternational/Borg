7c4dae2de3bb5a29e8ab9bb2968c2bb8db91fcd4
Greg Clarke <gclarke@amnesty.ca>
2013-05-10 17:21:07 -0400

    its friday :)

--- engaging_networks.rb
+++ engaging_networks.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'yaml'
 
 token = YAML::load(File.open('yaml/api_tokens.yml'))['api_tokens']['engagingnetworkstoken']
-startdate = (Time.now - (1 * 24 * 60 * 60)).strftime("%m%d%Y") # one days worth
+startdate = (Time.now - (2 * 24 * 60 * 60)).strftime("%m%d%Y") # one days worth
 
 puts "Requesting records with : http://www.e-activist.com/ea-dataservice/export.service?token=#{token}&startDate=#{startdate}&type=xml"
 
@@ -14,8 +14,6 @@ response = http.request(Net::HTTP::Get.new("/ea-dataservice/export.service?token
 endata = XmlSimple.xml_in(response.body.force_encoding("ISO-8859-1").encode("UTF-8"), { 'KeyAttr' => 'name' })['rows'][0]['row']
 
 puts "" + endata.length.to_s + " records imported..."
-
-sleep 5
 
 endata.each do | row |
 puts row.inspect + "\n"
@@ -61,4 +59,6 @@ puts row.inspect + "\n"
   puts "title: " + row['title'][0]
 =end
 end
+
+puts "" + endata.length.to_s + " records imported..."
 
