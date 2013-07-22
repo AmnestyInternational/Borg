c5a4b0ddb1b7d75f186205e11d43ee1aabcf5139
gregology <greg@gho.st>
2013-07-22 11:00:09 -0400

    added users for campaigns

--- tweetsFromCampaigns.rb
+++ tweetsFromCampaigns.rb
@@ -49,6 +49,49 @@ def pull_tweets
 
         end
       end
+
+      totalusers = campaigndetails['Users'].length
+      log_time("#{totalusers} users in Yaml file")
+      count = 0
+
+      campaigndetails['Users'].each do | screen_name |
+        max_id = nil
+        count += 1
+        user_details = lookup_twitter_user(screen_name)
+        since_id = fetch_user_since_id(screen_name)
+        log_time("#{screen_name} is #{count} of #{totalusers}")
+        log_time("#{screen_name} has #{user_details['statuses_count']} tweets, collecting...")
+
+        tweetscount = 0
+
+        loop do
+
+          tweetdata = fetch_user_timeline(screen_name, since_id, max_id)
+          max_id = tweetdata['max_id']
+
+          tweetscount += tweetdata['tweets'].length if tweetdata['tweets'].length > 0
+
+          insert_tweets(tweetdata['tweets']) if tweetdata['tweets'].length > 0
+          sleep 1
+          insert_twitter_users(tweetdata['twitterusers']) if tweetdata['twitterusers'].length > 0
+          sleep 1
+          insert_tweet_regions(tweetdata['tweetregions']) if tweetdata['tweetregions'].length > 0
+          sleep 1
+          insert_tweet_hashtags(tweetdata['tweethashtags']) if tweetdata['tweethashtags'].length > 0
+          sleep 1
+          insert_tweet_urls(tweetdata['tweeturls']) if tweetdata['tweeturls'].length > 0
+          sleep 1
+          insert_tweets_anatomized(tweetdata['tweetsanatomize']) if tweetdata['tweetsanatomize'].length > 0
+          sleep 1
+          insert_tweet_user_mentions(tweetdata['tweetusermentions']) if tweetdata['tweetusermentions'].length > 0
+
+          break if tweetdata["tweets"].empty?
+          log_time("#{tweetscount} collected, sleeping for 60 seconds...")
+          sleep 60
+        end
+        log_time("#{tweetscount} collected for #{screen_name}\n")
+      end
+
     end
   end
 end
