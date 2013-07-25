4206632cf4d2821d06a88273e150c6b1efb11c3c
gregology <greg@gho.st>
2013-07-25 09:59:16 -0400

    fixed issue with user polling and improved log formatting

--- tweetsFromCampaigns.rb
+++ tweetsFromCampaigns.rb
@@ -16,82 +16,97 @@ def pull_tweets
 
     log_time("Campaign: #{campaignname}")
     log_time("Start = #{campaigndetails['start']}, end = #{campaigndetails['end']}, timezone = #{campaigndetails['timezone']}")
-    log_time("Current? #{campaigndetails.is_current?.to_s}")
+    log_time("Current campaign? #{campaigndetails.is_current?}\n\n")
 
     if campaigndetails.is_current?
 
-      campaigndetails['Search terms'].each do | search_term |
+      unless campaigndetails['Search terms'].nil?
 
-        @yml['Regions'].each do | region |
+        log_time("Collecting search terms\n\n")
 
-          regionname = region[0]
-          area = region[1]['areas'][0]
+        campaigndetails['Search terms'].each do | search_term |
 
-          since_id = get_since_id(regionname, search_term)
+          @yml['Regions'].each do | region |
 
-          log_time("polling: #{search_term} from #{regionname} since #{since_id}")
+            regionname = region[0]
+            area = region[1]['areas'][0]
 
-          tweetdata = fetch_tweets_from_area(regionname, area, since_id, search_term)
+            since_id = get_since_id(regionname, search_term)
 
-          insert_tweets(tweetdata['tweets']) if tweetdata['tweets'].length > 0
-          sleep 1
-          insert_twitter_users(tweetdata['twitterusers']) if tweetdata['twitterusers'].length > 0
-          sleep 1
-          insert_tweet_regions(tweetdata['tweetregions']) if tweetdata['tweetregions'].length > 0
-          sleep 1
-          insert_tweet_hashtags(tweetdata['tweethashtags']) if tweetdata['tweethashtags'].length > 0
-          sleep 1
-          insert_tweet_urls(tweetdata['tweeturls']) if tweetdata['tweeturls'].length > 0
-          sleep 1
-          insert_tweets_anatomized(tweetdata['tweetsanatomize']) if tweetdata['tweetsanatomize'].length > 0
-          sleep 1
-          insert_tweet_user_mentions(tweetdata['tweetusermentions']) if tweetdata['tweetusermentions'].length > 0
+            log_time("Polling: #{search_term} from #{regionname} since #{since_id}")
 
+            tweetdata = fetch_tweets_from_area(regionname, area, since_id, search_term)
+
+            insert_tweets(tweetdata['tweets']) if tweetdata['tweets'].length > 0
+            sleep 1
+            insert_twitter_users(tweetdata['twitterusers']) if tweetdata['twitterusers'].length > 0
+            sleep 1
+            insert_tweet_regions(tweetdata['tweetregions']) if tweetdata['tweetregions'].length > 0
+            sleep 1
+            insert_tweet_hashtags(tweetdata['tweethashtags']) if tweetdata['tweethashtags'].length > 0
+            sleep 1
+            insert_tweet_urls(tweetdata['tweeturls']) if tweetdata['tweeturls'].length > 0
+            sleep 1
+            insert_tweets_anatomized(tweetdata['tweetsanatomize']) if tweetdata['tweetsanatomize'].length > 0
+            sleep 1
+            insert_tweet_user_mentions(tweetdata['tweetusermentions']) if tweetdata['tweetusermentions'].length > 0
+
+            log_time("Finished polling: #{search_term} from #{regionname}\n\n")
+
+          end
         end
+        log_time("Finished collecting search terms\n\n\n")
       end
 
-      totalusers = campaigndetails['Users'].length
-      log_time("#{totalusers} users in Yaml file")
-      count = 0
+      unless campaigndetails['Users'].nil?
 
-      campaigndetails['Users'].each do | screen_name |
-        max_id = nil
-        count += 1
-        user_details = lookup_twitter_user(screen_name)
-        since_id = fetch_user_since_id(screen_name)
-        log_time("#{screen_name} is #{count} of #{totalusers}")
-        log_time("#{screen_name} has #{user_details['statuses_count']} tweets, collecting...")
+        log_time("Collecting users tweets\n\n")
 
-        tweetscount = 0
+        totalusers = campaigndetails['Users'].length
+        log_time("#{totalusers} users in Yaml file")
+        count = 0
 
-        loop do
+        campaigndetails['Users'].each do | screen_name |
+          max_id = nil
+          count += 1
+          user_details = lookup_twitter_user(screen_name)
+          log_time("User does not exist", "error") if user_details.nil?
+          next if user_details.nil?
+          since_id = fetch_user_since_id(user_details)
+          log_time("#{user_details[:screen_name]} is #{count} of #{totalusers}")
+          log_time("#{user_details[:screen_name]} has #{user_details[:statuses_count].to_s} tweets, collecting...")
 
-          tweetdata = fetch_user_timeline(screen_name, since_id, max_id)
-          max_id = tweetdata['max_id']
+          tweetscount = 0
 
-          tweetscount += tweetdata['tweets'].length if tweetdata['tweets'].length > 0
+          loop do
 
-          insert_tweets(tweetdata['tweets']) if tweetdata['tweets'].length > 0
-          sleep 1
-          insert_twitter_users(tweetdata['twitterusers']) if tweetdata['twitterusers'].length > 0
-          sleep 1
-          insert_tweet_regions(tweetdata['tweetregions']) if tweetdata['tweetregions'].length > 0
-          sleep 1
-          insert_tweet_hashtags(tweetdata['tweethashtags']) if tweetdata['tweethashtags'].length > 0
-          sleep 1
-          insert_tweet_urls(tweetdata['tweeturls']) if tweetdata['tweeturls'].length > 0
-          sleep 1
-          insert_tweets_anatomized(tweetdata['tweetsanatomize']) if tweetdata['tweetsanatomize'].length > 0
-          sleep 1
-          insert_tweet_user_mentions(tweetdata['tweetusermentions']) if tweetdata['tweetusermentions'].length > 0
+            tweetdata = fetch_user_timeline(screen_name, since_id, max_id)
+            max_id = tweetdata['max_id']
 
-          break if tweetdata["tweets"].empty?
-          log_time("#{tweetscount} collected, sleeping for 60 seconds...")
-          sleep 60
+            tweetscount += tweetdata['tweets'].length if tweetdata['tweets'].length > 0
+
+            insert_tweets(tweetdata['tweets']) if tweetdata['tweets'].length > 0
+            sleep 1
+            insert_twitter_users(tweetdata['twitterusers']) if tweetdata['twitterusers'].length > 0
+            sleep 1
+            insert_tweet_regions(tweetdata['tweetregions']) if tweetdata['tweetregions'].length > 0
+            sleep 1
+            insert_tweet_hashtags(tweetdata['tweethashtags']) if tweetdata['tweethashtags'].length > 0
+            sleep 1
+            insert_tweet_urls(tweetdata['tweeturls']) if tweetdata['tweeturls'].length > 0
+            sleep 1
+            insert_tweets_anatomized(tweetdata['tweetsanatomize']) if tweetdata['tweetsanatomize'].length > 0
+            sleep 1
+            insert_tweet_user_mentions(tweetdata['tweetusermentions']) if tweetdata['tweetusermentions'].length > 0
+
+            break if tweetdata["tweets"].empty?
+            log_time("#{tweetscount} collected, sleeping for 60 seconds...")
+            sleep 60
+          end
+          log_time("#{tweetscount} collected for #{screen_name}\n\n")
         end
-        log_time("#{tweetscount} collected for #{screen_name}\n")
+        log_time("Finished collecting users tweets\n\n")
       end
-
     end
   end
 end
@@ -99,4 +114,4 @@ end
 setvars
 pull_tweets
 
-log_time("End time\n\n\n")
+log_time("End time\n\n\n\n")
